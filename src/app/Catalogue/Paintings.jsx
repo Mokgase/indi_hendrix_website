25b35ba1037f8d60dd4c206dc9d3549864c1eaa3
Mokgase <thapelommakola2@gmail.com>
2025-01-23 14:31:57 +0200

    paintings component

--- src/app/Catalogue/Paintings.jsx
+++ src/app/Catalogue/Paintings.jsx
@@ -27,16 +27,18 @@ import styles from '../Style/paintings.module.css';
 
 const Paintings = () => {
   const cataloguePaintings = [
+    '/Assets/images/Golden_Years_official.png',
+    'Assets/images/FEAR_OF_GROWTH.png',
     '/Assets/images/45_Hours.png',
+    '/Assets/images/Testament_of_unkillable_gods.png',
     '/Assets/images/Can _I_get_a_light.jpg',
-    '/Assets/images/Doomile.png',
     '/Assets/images/faces-phases.jpg',
     '/Assets/images/Fantastic_planet.jpg',
     '/Assets/images/Father_Stretch_my_Hand.jpg',
     '/Assets/images/Rakgolo.jpg',
     '/Assets/images/Scrying_Mirror.jpg',
-    '/Assets/images/Testament_of_unkillable_gods.png',
     '/Assets/images/This_depressions_not_a_phase.jpg',
+    '/Assets/images/Doomile.png',
   ];
 
   return (
