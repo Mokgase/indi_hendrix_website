7706d7ae2786497574e13eceb9f81806a1e7479f
Thapelo Mmakola <thapelommakola2@gmail.com>
2025-06-20 00:19:30 +0200

    new painting

--- src/app/Catalogue/Paintings.jsx
+++ src/app/Catalogue/Paintings.jsx
@@ -6,6 +6,7 @@ import Head from 'next/head';
 
 const cataloguePaintings = [
   
+  { src: '/Assets/images/When_trees_fall.png', title: 'When trees fall', size: '600 x 900 mm', year:'2025', price: 'R2700.00' },
   { src: '/Assets/images/I_carry_them_with_me.png', title: 'I carry them with me', size: '600 x 900 mm', year:'2025', price: 'R2500.00' },
   { src: '/Assets/images/Let_them_eat_Brioche.png', title: 'Let them eat Brioche', size: '600 x 900 mm', year:'2025', price: 'SOLD' },
   { src: '/Assets/images/Golden_Years_official.png', title: 'Golden Years', size: '594 x 841 mm', year:'2025', price: 'R2000.00' },
