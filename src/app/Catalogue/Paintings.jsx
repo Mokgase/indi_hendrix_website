3585411eb7d3fa5f2230280c700535e207ff0dfa
Thapelo Mmakola <thapelommakola2@gmail.com>
2025-06-22 13:48:34 +0200

    untitled 25 painting added

--- src/app/Catalogue/Paintings.jsx
+++ src/app/Catalogue/Paintings.jsx
@@ -9,6 +9,7 @@ const cataloguePaintings = [
   { src: '/Assets/images/When_trees_fall.png', title: 'When trees fall', size: '600 x 900 mm', year:'2025', price: 'R2700.00' },
   { src: '/Assets/images/I_carry_them_with_me.png', title: 'I carry them with me', size: '600 x 900 mm', year:'2025', price: 'R2500.00' },
   { src: '/Assets/images/Let_them_eat_Brioche.png', title: 'Let them eat Brioche', size: '600 x 900 mm', year:'2025', price: 'SOLD' },
+  { src: '/Assets/images/Untitled_25.png', title: 'Untitled 25', size: '594 x 841 mm', year:'2025', price: 'R1500.00' },
   { src: '/Assets/images/Golden_Years_official.png', title: 'Golden Years', size: '594 x 841 mm', year:'2025', price: 'R2000.00' },
   { src: '/Assets/images/FEAR_OF_GROWTH.png', title: 'Fear of Growth', size: '594 x 841 mm', year:'2024', price: 'R2000.00' },
   { src: '/Assets/images/45_Hours.png', title: '45 Hours', size: '594 x 841 mm', year:'2024', price: 'R2000.00' },
