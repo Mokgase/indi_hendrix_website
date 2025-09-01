1def02386110950c317703496e90c642ec1d78c1
Thapelo Mmakola <thapelommakola2@gmail.com>
2025-09-01 22:56:52 +0200

    updated paintings

--- src/app/Utils/paintings.js
+++ src/app/Utils/paintings.js
@@ -1,15 +1,15 @@
 export const cataloguePaintings = [
   
-  { src: '/Assets/images/I_carry_them_with_me.png', title: 'I carry them with me', size: '600 x 900 mm', year:'2025', price: 'R2500.00' },
+  { src: '/Assets/images/I_carry_them_with_me.png', title: 'I carry them with me', size: '600 x 900 mm', year:'2025', price: 'SOLD' },
   { src: '/Assets/images/Let_them_eat_Brioche.png', title: 'Let them eat Brioche', size: '600 x 900 mm', year:'2025', price: 'SOLD' },
   { src: '/Assets/images/Golden_Years_official.png', title: 'Golden Years', size: '594 x 841 mm', year:'2025', price: 'R2000.00' },
   { src: '/Assets/images/FEAR_OF_GROWTH.png', title: 'Fear of Growth', size: '594 x 841 mm', year:'2024', price: 'R2000.00' },
   { src: '/Assets/images/45_Hours.png', title: '45 Hours', size: '594 x 841 mm', year:'2024', price: 'R2000.00' },
-  { src: '/Assets/images/Testament_of_unkillable_gods.png', title: 'Testament of Unkillable Gods', size: '420 x 594 mm', year:'2024', price: 'R1200.00' },
+  { src: '/Assets/images/Testament_of_unkillable_gods.png', title: 'Testament of Unkillable Gods', size: '420 x 594 mm', year:'2024', price: 'SOLD' },
   { src: '/Assets/images/Can_I_get_a_light.png', title: 'Can I Get a Light', size: '297 x 420 mm', year:'2023', price: 'R500.00' },
   { src: '/Assets/images/Doomile.png', title: 'Doomile', size: '594 x 841 mm', year:'2023', price: 'TBD' },
   { src: '/Assets/images/Scrying_Mirror.jpg', title: 'Scrying Mirror', size: '594 x 841 mm', year:'2023', price: 'TBD' },
-  { src: '/Assets/images/Rakgolo.png', title: 'Rakgolo', size: '36x36 inches', year:'2023', price: 'R400.00' },
+  { src: '/Assets/images/Rakgolo.png', title: 'Rakgolo', size: '36x36 inches', year:'2023', price: 'SOLD' },
   { src: '/Assets/images/Father_Stretch_my_Hand.jpg', title: 'Father Stretch My Hand', size: '594 x 841 mm', year:'2023', price: 'TBD' },
   { src: '/Assets/images/faces-phases.png', title: 'Faces & Phases', size: '297 x 420 mm', year:'2022', price: 'R600.00' },
   { src: '/Assets/images/This_depressions_not_a_phase.png', title: 'This Depression is Not a Phase', size: '297 x 420 mm', year:'2022', price: 'SOLD' },
