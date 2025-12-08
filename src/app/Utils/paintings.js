3884f360a8f65ce6e2c37bdd2f8220eb0c3f9be9
Thapelo Mmakola <thapelommakola2@gmail.com>
2025-12-09 00:06:13 +0200

    updated the painting price

--- src/app/Utils/paintings.js
+++ src/app/Utils/paintings.js
@@ -7,7 +7,7 @@ const cataloguePaintings = [
   { src: '/Assets/images/monna_wa_go_šoma_III.png', title: 'Monna wa go soma III', size: '260 x 360 mm', year:'2025', price: 'R2000.00', story:'Acrylic paint & oil pastels on canvas' },
   { src: '/Assets/images/bree_st.png', title: 'Bree St.', size: '600 x 900 mm', year:'2025', price: 'R3000.00', story:'Acrylic paint & oil pastels on canvas' },
   { src: '/Assets/images/the_strength_behind_him.png', title: 'The Strength Behind HIM', size: '615 x 920 mm', year:'2025', price: 'R4700.00', story:'Acrylic paint & oil pastels on canvas' },
-  { src: '/Assets/images/war_ready.png', title: 'War Ready', size: '615 x 917 mm', year:'2025', price: 'R5000.00', story:'Acrylic paint & oil pastels on canvas' },
+  { src: '/Assets/images/war_ready.png', title: 'War Ready', size: '615 x 917 mm', year:'2025', price: 'SOLD', story:'Acrylic paint & oil pastels on canvas' },
   { src: '/Assets/images/When_trees_fall.png', title: 'When trees fall', size: '600 x 900 mm', year:'2025', price: 'R3000.00', story:'Acrylic paint & oil pastels on canvas' },
   { src: '/Assets/images/I_carry_them_with_me.png', title: 'I carry them with me', size: '600 x 900 mm', year:'2025', price: 'SOLD' },
   { src: '/Assets/images/Let_them_eat_Brioche.png', title: 'Let them eat Brioche', size: '600 x 900 mm', year:'2025', price: 'SOLD' },
